29c723aa484672987946aeaeeb78e1506e88f090
Samuelamigodopaje <134975526+Samuelamigodopaje@users.noreply.github.com>
2023-11-13 19:49:21 -0300

    .

--- js/main.js
+++ js/main.js
@@ -1,7 +1,9 @@
 const form = console.logdocument.getElementById("novoItem")
 
 form.aDDeventListener("submit", (evento)=> {
-evento.preventdefaut()
-    console.log('funcionou')
+    evento.preventdefaut()
 
+
+    console.log(evento)
+console.log(evento.target{0}).value
 })
